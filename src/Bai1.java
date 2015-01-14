cc7ecc6bd3e1a104b32578b71c2119198e1f19c6
TranVanCuong <trancuongit1994@gmail.com>
2015-01-14 15:33:09 +0700

    Tuan 1

---
+++ src/Bai1.java
@@ -0,0 +1,14 @@
+
+public class Bai1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int intNumber = 0;
+		double doubleNumber = 7.9;
+		intNumber = (int)doubleNumber ;
+		System.out.println("intNumber :" + intNumber);
+	}
+
+}
